d865bea58284b9d23bdae667677002e9b89ae3aa
zhoucan1 <zhoucan1@360.cn>
2019-12-03 18:03:26 +0800

    element-ui插件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import '@src/plugins/elementUi';
 
 Vue.config.productionTip = false;
 
