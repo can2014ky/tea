9c58b331aa2fe8bd0d539f28cf57564223b7933c
zhoucan1 <zhoucan1@360.cn>
2019-12-04 11:29:15 +0800

    404页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/home/Home.vue';
+import Error from '../views/error/404.vue';
 
 Vue.use(VueRouter);
 
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/about/About.vue'),
+  },
+  {
+    path: '*',
+    name: 'error',
+    component: Error,
   },
 ];
 
