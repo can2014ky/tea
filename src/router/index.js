4299330f083fa44cf126c331d07ee844f94d1559
zhoucan1 <zhoucan1@360.cn>
2019-12-04 16:32:04 +0800

    不同页面，网页显示不同 title 标题

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import { setTitle } from '@src/utils/setTitle';
 import routes from './router';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    setTitle(to.meta.title);
+  }
+  next();
 });
 
 export default router;
