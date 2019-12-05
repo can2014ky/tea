9355aa4601b447744870f32950b810361b0e3608
zhoucan1 <zhoucan1@360.cn>
2019-12-05 17:29:41 +0800

    自动化注册全局组件 require.context

--- src/views/menu2/eat/Potato.vue
+++ src/views/menu2/eat/Potato.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <siemPagination></siemPagination>
+    <siemDialog :dialogConfig="dialogConfig"></siemDialog>
     土豆
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      dialogConfig: {
+        dialogVisible: true,
+      },
+    };
+  },
+};
+</script>
