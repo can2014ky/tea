f22dc4812744988461416fd969ea1c2a0ca5a973
zhoucan1 <zhoucan1@360.cn>
2019-12-05 18:37:12 +0800

    svgo优化svg文件

--- src/views/menu2/eat/Potato.vue
+++ src/views/menu2/eat/Potato.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     土豆
+    <svg-icon icon-class="assetsConfig"></svg-icon>
     <svg-icon icon-class="pdf" class="pdf"></svg-icon>
     <siemPagination></siemPagination>
     <siemDialog :dialogConfig="dialogConfig"></siemDialog>
