5bb790b03765ef66ed1c1b4e0e91ff01d5afb33b
zhoucan1 <zhoucan1@360.cn>
2019-12-09 16:13:13 +0800

    vuex-persistedstate数据持久化存储

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 import site from './modules/site';
 
 Vue.use(Vuex);
@@ -14,4 +15,5 @@ export default new Vuex.Store({
   modules: {
     site,
   },
+  plugins: [createPersistedState()],
 });
