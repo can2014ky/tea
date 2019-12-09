1f457d9835072b6ef94e8d45b4b6ad5c3c838008
zhoucan1 <zhoucan1@360.cn>
2019-12-09 18:52:56 +0800

    lodash按需引入(比较low的方式)

--- src/views/menu1/home/Home.vue
+++ src/views/menu1/home/Home.vue
@@ -6,11 +6,15 @@
 
 <script>
 import { getBanner } from '@src/api/request/menu1';
+import cloneDeep from 'lodash/cloneDeep';
 
 export default {
   name: 'home',
   created() {
     getBanner();
+    const objects = [{ a: 1 }, { b: 2 }];
+    const deep = cloneDeep(objects);
+    console.log(333, deep);
   },
 };
 </script>
