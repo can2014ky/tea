029cdbfdbd57cb0eaa922491010bdf1d536f71ef
zhoucan1 <zhoucan1@360.cn>
2019-12-11 14:09:21 +0800

    按需引入全部element-ui组件

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
+
 import {
-  Pagination,
   Dialog,
-  Autocomplete,
   Dropdown,
   DropdownMenu,
   DropdownItem,
@@ -11,127 +10,134 @@ import {
   MenuItem,
   MenuItemGroup,
   Input,
-  InputNumber,
-  Radio,
-  RadioGroup,
-  RadioButton,
-  Checkbox,
-  CheckboxButton,
-  CheckboxGroup,
-  Switch,
   Select,
   Option,
-  OptionGroup,
   Button,
-  ButtonGroup,
   Table,
   TableColumn,
-  DatePicker,
-  TimeSelect,
-  TimePicker,
-  Popover,
-  Tooltip,
-  Breadcrumb,
-  BreadcrumbItem,
-  Form,
-  FormItem,
   Tabs,
   TabPane,
   Tag,
   Tree,
-  Alert,
-  Slider,
-  Icon,
-  Row,
-  Col,
-  Upload,
-  Progress,
   Badge,
-  Card,
-  Rate,
-  Steps,
-  Step,
-  Carousel,
-  CarouselItem,
-  Collapse,
-  CollapseItem,
-  Cascader,
-  ColorPicker,
-  Transfer,
   Container,
   Header,
   Aside,
   Main,
-  Footer,
   Loading,
   MessageBox,
   Message,
   Notification,
+  Avatar,
+  Card,
+  Form,
+  FormItem,
+  Switch,
+  Checkbox,
+  CheckboxGroup,
+  Upload,
+  TimePicker,
+  DatePicker,
+  Popover,
+  Drawer,
+  Pagination,
+  ColorPicker,
+  RadioButton,
+  RadioGroup,
+  Radio,
+  Link,
+  InputNumber,
+  Transfer,
+  Row,
+  Col,
+  Scrollbar,
+  Collapse,
+  CollapseItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Autocomplete,
+  PageHeader,
+  Divider,
+  Tooltip,
+  Progress,
+  ButtonGroup,
+  Steps,
+  Step,
+  Alert,
+  Cascader,
+  OptionGroup,
 } from 'element-ui';
 
-Vue.use(Pagination);
-Vue.use(Dialog);
-Vue.use(Autocomplete);
-Vue.use(Dropdown);
-Vue.use(DropdownMenu);
-Vue.use(DropdownItem);
-Vue.use(Menu);
-Vue.use(Submenu);
-Vue.use(MenuItem);
-Vue.use(MenuItemGroup);
-Vue.use(Input);
-Vue.use(InputNumber);
-Vue.use(Radio);
-Vue.use(RadioGroup);
-Vue.use(RadioButton);
-Vue.use(Checkbox);
-Vue.use(CheckboxButton);
-Vue.use(CheckboxGroup);
-Vue.use(Switch);
-Vue.use(Select);
-Vue.use(Option);
-Vue.use(OptionGroup);
-Vue.use(Button);
-Vue.use(ButtonGroup);
-Vue.use(Table);
-Vue.use(TableColumn);
-Vue.use(DatePicker);
-Vue.use(TimeSelect);
-Vue.use(TimePicker);
-Vue.use(Popover);
-Vue.use(Tooltip);
-Vue.use(Breadcrumb);
-Vue.use(BreadcrumbItem);
-Vue.use(Form);
-Vue.use(FormItem);
-Vue.use(Tabs);
-Vue.use(TabPane);
-Vue.use(Tag);
-Vue.use(Tree);
-Vue.use(Alert);
-Vue.use(Slider);
-Vue.use(Icon);
-Vue.use(Row);
-Vue.use(Col);
-Vue.use(Upload);
-Vue.use(Progress);
-Vue.use(Badge);
-Vue.use(Card);
-Vue.use(Rate);
-Vue.use(Steps);
-Vue.use(Step);
-Vue.use(Carousel);
-Vue.use(CarouselItem);
-Vue.use(Collapse);
-Vue.use(CollapseItem);
-Vue.use(Cascader);
-Vue.use(ColorPicker);
-Vue.use(Transfer);
-Vue.use(Container);
+Vue.prototype.$ELEMENT = { size: 'small', zIndex: 3000 };
+
 Vue.use(Header);
 Vue.use(Aside);
 Vue.use(Main);
-Vue.use(Footer);
+Vue.use(Container);
+Vue.use(Tabs);
+Vue.use(TabPane);
+Vue.use(Card);
+Vue.use(Breadcrumb);
+Vue.use(BreadcrumbItem);
+Vue.use(PageHeader);
+Vue.use(Steps);
+Vue.use(Step);
+
+Vue.use(MenuItem);
+Vue.use(MenuItemGroup);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+
+Vue.use(Avatar);
+Vue.use(Badge);
+Vue.use(Tag);
+
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Select);
+Vue.use(Button);
+Vue.use(Option);
+Vue.use(OptionGroup);
+Vue.use(Switch);
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
+Vue.use(Radio);
+Vue.use(RadioButton);
+Vue.use(RadioGroup);
+Vue.use(Upload);
+Vue.use(TimePicker);
+Vue.use(DatePicker);
+Vue.use(ColorPicker);
+Vue.use(Link);
+Vue.use(InputNumber);
+Vue.use(Transfer);
+Vue.use(ButtonGroup);
+Vue.use(Cascader);
+
+Vue.use(Dialog);
+Vue.use(Popover);
+Vue.use(Drawer);
+Vue.use(Tooltip);
+Vue.use(Progress);
+Vue.use(Alert);
+
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Tree);
+
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Scrollbar);
+Vue.use(Collapse);
+Vue.use(CollapseItem);
+Vue.use(Pagination);
+
+Vue.use(Autocomplete);
+Vue.use(Divider);
 
 Vue.use(Loading.directive);
 
