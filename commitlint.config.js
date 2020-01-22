e64b6fd08065fc97ae20af22a74008beceb89c4b
zhoucan1 <zhoucan1@360.cn>
2020-01-22 16:52:42 +0800

    chore: commitlint 代码提交检测

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+// 方法一：安装 npm install --save-dev @commitlint/config-conventional @commitlint/cli
+// 生成配置文件commitlint.config.js 或 .commitlintrc.js
+// echo "module.exports = {extends: ['@commitlint/config-conventional']};" > commitlint.config.js
+
+module.exports = { extends: ['@commitlint/config-conventional'] };
+
+// 方法二：定制提交规范
+// module.exports = {
+//   extends: ['@commitlint/config-conventional'],
+//   rules: {
+//     'type-enum': [
+//       2,
+//       'always',
+//       ['upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert'],
+//     ],
+//     'type-case': [0],
+//     'type-empty': [0],
+//     'scope-empty': [0],
+//     'scope-case': [0],
+//     'subject-full-stop': [0, 'never'],
+//     'subject-case': [0, 'never'],
+//     'header-max-length': [0, 'always', 72],
+//   },
+// };
